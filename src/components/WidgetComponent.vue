48adc64fa9e1d9b5709d04f007fe637a6a1f6a4a
maksalexsavchenko <makskidexom@gmail.com>
2023-06-01 15:41:56 +0300

    add files

---
+++ src/components/WidgetComponent.vue
@@ -0,0 +1,35 @@
+<template>
+<div class="widget"
+    :style="{
+    'background-color': backgroundColor
+    }"
+    >
+    <input v-model="name" type="text" placeholder="Coin" @blur="showValue">
+</div>
+</template>
+
+<script >
+export default {
+    name: 'WidgetComponent',
+    data() {
+        return {
+            name: '',
+            backgroundColor: ''
+        }
+    },
+    mounted() {
+        let q = this.$route.query
+        this.name = q.coin
+    },
+    methods: {
+        showValue() {
+            alert(this.name)
+        }
+    }
+
+}
+</script>
+
+<style >
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,25 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import {createRouter, createWebHistory} from "vue-router";
+import WidgetComponent from "./components/WidgetComponent.vue";
+
+const routes = [
+    {
+        name: 'Home',
+        path: '/',
+        component: WidgetComponent
+    }
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+const app = createApp(App)
+
+
+app.use(router)
+
+app.mount('#app')
